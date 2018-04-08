219480d4ee415b815d798433d3bd73f098584a57
Kim Yong Ho <kimyh1981@gmail.com>
2018-04-08 23:30:34 +0900

    Updated signup.

--- MySmallTrip/MySmallTrip/SignUpViewController.swift
+++ MySmallTrip/MySmallTrip/SignUpViewController.swift
@@ -9,8 +9,9 @@
 import UIKit
 import Alamofire
 
-class SignUpViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+class SignUpViewController: UIViewController {
+    
+    //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         Alamofire.request("http://myrealtrip.hongsj.kr")
@@ -23,48 +24,66 @@ class SignUpViewController: UIViewController, UIImagePickerControllerDelegate, U
                 }
         }
         
+        //MARK: - 바탕 터치시, 키보드 사라지기
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard(_:)))
+        contentView.addGestureRecognizer(tap)
+        
+        
         yourNameTextField.delegate = self
         yourEmailTextField.delegate = self
         passwordConfirmTextField.delegate = self
         passwordTextField.delegate = self
         phoneNumberTextField.delegate = self
-        imagePicker.delegate = self
         
-        let tap = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard(_:)))
-        viewTouch.addGestureRecognizer(tap)
         createUI()
+        setupProfileImage()
     }
+    
 
-    //MARK: - head label
+    //MARK: - Head label
     @IBOutlet weak var singUpLabel: UILabel!
     @IBOutlet weak var greetingLabel: UILabel!
     
-    //MARK: - profile image 입력부
-    //Profile 사진 이미지 피커로 구현하기: 사진 라이브러리 -> URL 경로 전달
+    //MARK: - Profile image 입력부
+    //Profile 사진 이미지 피커로 구현하기: 사진 라이브러리 -> URL 경로 전달 (구현 못함)
     @IBOutlet weak var profileImage: UIImageView!
     let imagePicker = UIImagePickerController()
     
     @IBAction func profileImageButton(_ sender: UIButton) {
-        imagePicker.allowsEditing = false
+        imagePicker.allowsEditing = true
+        imagePicker.delegate = self
+        
+        let alert = UIAlertController(title: "프로필사진", message: "원하는 사진을 넣어 주세요", preferredStyle: .actionSheet)
+        let library = UIAlertAction(title: "사진앨범", style: .default) { (action) in
+            self.openLibrary()
+        }
+        let camera = UIAlertAction(title: "카메라", style: .default) { (action) in
+            self.openCamera()
+        }
+        let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        alert.addAction(library)
+        alert.addAction(camera)
+        alert.addAction(cancel)
+        present(alert, animated: true, completion: nil)
+    }
+    func openLibrary() {
         imagePicker.sourceType = .photoLibrary
-        present(imagePicker, animated: true, completion: nil)
+        present(imagePicker, animated: false, completion: nil)
     }
     
-//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-//        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
-//            profileImage.contentMode = .scaleAspectFit
-//            profileImage.image = pickedImage
-//        }
-//        dismissedViewControllerAnimated(true, completion: nil)
-//    }
-//
-//    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-//        dismissedViewControllerAnimated(true, completion: nil)
-//    }
+    func openCamera() {
+        if(UIImagePickerController.isSourceTypeAvailable(.camera)) {
+            imagePicker.sourceType = .camera
+            present(imagePicker, animated: false, completion: nil)
+        } else {
+            print("Camera is not available.")
+        }
+    }
     
     
-    //MARK: - TextField 입력부
-    @IBOutlet weak var viewTouch: UIView!
+    //MARK: - TextField 및 View
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var contentView: UIView!
     @IBOutlet weak var yourNameTextField: UITextField!
     @IBOutlet weak var yourEmailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
@@ -114,6 +133,7 @@ class SignUpViewController: UIViewController, UIImagePickerControllerDelegate, U
         
     }
     
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -123,6 +143,7 @@ class SignUpViewController: UIViewController, UIImagePickerControllerDelegate, U
 //MARK: - TextReturn 구현부
 extension SignUpViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
         if self.yourEmailTextField.isFirstResponder {
             yourNameTextField.becomeFirstResponder()
         } else if self.yourNameTextField.isFirstResponder {
@@ -135,6 +156,24 @@ extension SignUpViewController: UITextFieldDelegate {
             phoneNumberTextField.resignFirstResponder()
         }
         return true
+    }
+}
+
+//MARK: - Profile image - UIImagePickerController
+extension SignUpViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            profileImage.image = image
+            print(info)
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    func setupProfileImage() {
+        profileImage.layer.cornerRadius = profileImage.frame.size.width/2
+        profileImage.clipsToBounds = true
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
     }
 }
 
