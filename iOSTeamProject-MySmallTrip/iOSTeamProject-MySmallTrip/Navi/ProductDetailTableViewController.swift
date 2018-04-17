471a9ebf30606f129c6224b711ee7375b9770802
sungeunDev <starfg62@gmail.com>
2018-04-17 17:17:25 +0900

    travel detail view - image pageVC was replaced imageVC. & add reservation button

--- iOSTeamProject-MySmallTrip/iOSTeamProject-MySmallTrip/Navi/ProductDetailTableViewController.swift
+++ iOSTeamProject-MySmallTrip/iOSTeamProject-MySmallTrip/Navi/ProductDetailTableViewController.swift
@@ -11,20 +11,25 @@ import Alamofire
 
 class ProductDetailTableViewController: UITableViewController {
     
-    @IBOutlet private weak var travelImagesHeaderView: TravelImagesHeaderView!
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var productNumber: UILabel!
+    var reservationButton: UIButton!
+    
     var productDetail: TravelDetail?
     
     struct StoryBoard {
-        static let showImagesPageViewController = "ShowImagesPageViewController"
         static let infoCell = "InfoCell"
         static let photoCell = "PhotoCell"
         static let descriptionCell = "DescriptionCell"
         static let companyCell = "CompanyCell"
+        
+        static let reservationVC = "ReservationViewController"
     }
     
     var url: String? {
         willSet(newValue) {
 
+            // 이전 페이지에서 받은 url로 데이터 불러오기
             Alamofire
                 .request(newValue!, method: .get)
                 .responseData { (response) in
@@ -34,14 +39,22 @@ class ProductDetailTableViewController: UITableViewController {
                         do {
                             let productDetailList  = try JSONDecoder().decode([TravelDetail].self, from: value)
                             self.productDetail = productDetailList[0]
+                            self.productNumber.text = "상품번호: \(self.productDetail!.pk)"
                             
-                            print("이게 우선?")
-                            self.performSegue(withIdentifier: StoryBoard.showImagesPageViewController, sender: nil)
-                            print("됨?")
-                            
-                            
-                            self.tableView.reloadData()
-
+                            // image insert
+                            Alamofire
+                                .request(self.productDetail!.images[0].productImg)
+                                .responseData { (response) in
+                                    
+                                    switch response.result {
+                                    case .success(let value):
+                                        self.imageView.image = UIImage(data: value)
+                                        self.tableView.reloadData()
+                                    case .failure(let error):
+                                        print("\n---------- [ cityImage load fail ] -----------\n")
+                                        print(error.localizedDescription)
+                                    }
+                            }
                         } catch(let error) {
                             print("\n---------- [ JSON Decoder error ] -----------\n")
                             print(error)
@@ -60,37 +73,65 @@ class ProductDetailTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // tableView cell height - text length에 따라 맞추기
         self.tableView.estimatedRowHeight = self.tableView.rowHeight
         self.tableView.rowHeight = UITableViewAutomaticDimension
- 
+        
+        // 예약하기 버튼
+        makeReserveBtn()
     }
-
-
-    // MARK:  - Navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-
-        if segue.identifier == StoryBoard.showImagesPageViewController {
-            if let imagePageVC = segue.destination as? TravelImagesPageViewController {
-                
-                print("이게 언제 호출됨?")
-                imagePageVC.images = self.productDetail?.images
-                imagePageVC.pageViewControllerDelegate = travelImagesHeaderView
-            }
-        }
+    
+    
+    // reservation Button layout
+    func makeReserveBtn() {
+        
+        let height: CGFloat = 49
+        reservationButton = UIButton(frame: CGRect(x: 0,
+                                                   y: self.view.frame.height - height * 2,
+                                                   width: self.view.frame.width,
+                                                   height: 49))
+        
+        reservationButton.backgroundColor = UIColor.Custom.mainColor
+        reservationButton.alpha = 0.95
+        
+        reservationButton.setTitle("예약하기", for: .normal)
+        reservationButton.setTitleColor(.white, for: .normal)
+        
+        reservationButton.addTarget(self, action: #selector(moveReserveVC(_:)), for: .touchUpInside)
+        
+        // TableVC로 생성해서 테이블뷰 위에 위치시키려면 parent.view에 addSubview할 수밖에... ㅠㅜ
+        self.parent?.view.addSubview(reservationButton)
+        
+        // back 누르면 사라지게 해야됨 ㅠ.ㅜ
+//        reservationButton.removeFromSuperview()
     }
+    
+    
+    @objc func moveReserveVC(_ sender: UIButton) {
+        let storyBoard = UIStoryboard(name: "Root", bundle: nil)
+        let popUpVC = storyBoard.instantiateViewController(withIdentifier: StoryBoard.reservationVC) as! ReservationViewController
+        
+        
+        
+        self.present(popUpVC, animated: true, completion: nil)
+    }
+    
     
 }
 
 
+
+
+// MARK:  - TableView Delegate, Datasource
 extension ProductDetailTableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // 0 - travel info cell (name, price, ..)
-        // 1 - photo cell
-        // 2 - detail description cell
-        // 3 - company info cell
-        return 4
+        // 1 - detail description cell
+        // 2 - photo cell
+        return 3
     }
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
@@ -100,14 +141,12 @@ extension ProductDetailTableViewController {
             cell.travel = productDetail
             return cell
         case 1:
-            let cell = tableView.dequeueReusableCell(withIdentifier: StoryBoard.photoCell, for: indexPath)
-            return cell
-        case 2:
             let cell = tableView.dequeueReusableCell(withIdentifier: StoryBoard.descriptionCell, for: indexPath) as! TravelDescriptionCell
+            cell.titleLabel.text = productDetail?.description_title
             cell.descriptionLabel.text = productDetail?.description
             return cell
         default:
-            let cell = tableView.dequeueReusableCell(withIdentifier: StoryBoard.companyCell, for: indexPath)
+            let cell = tableView.dequeueReusableCell(withIdentifier: StoryBoard.photoCell, for: indexPath)
             return cell
         }
     }
--- iOSTeamProject-MySmallTrip/iOSTeamProject-MySmallTrip/Navi/travel detail cell/TravelDescriptionCell.swift
+++ iOSTeamProject-MySmallTrip/iOSTeamProject-MySmallTrip/Navi/travel detail cell/TravelDescriptionCell.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TravelDescriptionCell: UITableViewCell {
     
-    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     
     override func awakeFromNib() {
