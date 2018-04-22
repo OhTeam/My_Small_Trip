9760cc8be100962208d15d18811ed19d5a49b0db
Kim Yong Ho <kimyh1981@gmail.com>
2018-04-23 01:32:05 +0900

    Update calendar but ongoing.

--- iOSTeamProject-MySmallTrip/iOSTeamProject-MySmallTrip/CalendarViewController.swift
+++ iOSTeamProject-MySmallTrip/iOSTeamProject-MySmallTrip/CalendarViewController.swift
@@ -7,6 +7,12 @@
 //
 
 import UIKit
+import Alamofire
+
+struct ColorOfIsSelected {
+    static var red = #colorLiteral(red: 0.9490196078, green: 0.3607843137, blue: 0.3843137255, alpha: 1)
+
+}
 
 class CalendarViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
@@ -15,10 +21,10 @@ class CalendarViewController: UIViewController, UICollectionViewDataSource, UICo
         
         createUI()
         initializeView()
+        getReservationDateBoolean()
         
+//        profileImage.layer.cornerRadius = profileImage.frame.size.width / 2
     }
-    
-
     
     @IBOutlet weak var unavailableDateMarkLabel: NSLayoutConstraint!
     @IBOutlet weak var unavailableDateLabel: UILabel!
@@ -26,7 +32,15 @@ class CalendarViewController: UIViewController, UICollectionViewDataSource, UICo
     @IBOutlet weak var topViewOfCalendar: UIView!
     
     @IBOutlet weak var cancelButton: UIButton!
+    @IBAction func isSelectedCancelButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
     @IBOutlet weak var applyButton: UIButton!
+    @IBAction func applyDateForReservationButton(_ sender: Any) {
+        //날짜를 선택하고, 적용 버튼을 누르면 '2018 / 03 / 21' 형식으로 print 할 수 있도록 처리.
+    }
+    
     
     @IBOutlet weak var dayNameUIStackView: UIStackView!
     @IBOutlet weak var calendarCollectionView: UICollectionView!
@@ -95,19 +109,16 @@ class CalendarViewController: UIViewController, UICollectionViewDataSource, UICo
 //        <#code#>
 //    }
 
-    
-    
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         
         let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "Today", for: indexPath) as! MonthHeaderView
-//        header.todayButton.setTitle("\(currentYear)년 \(presentMonthIndex)월", for: .normal)
-        
-        
+        //달을 April로 표기하기
         let date = Date()
         let formatter = DateFormatter()
         formatter.dateFormat = "MMMM"
         let result = formatter.string(from: date)
         header.todayButton.setTitle("\(result)", for: .normal)
+        //        header.todayButton.setTitle("\(currentYear)년 \(presentMonthIndex)월", for: .normal)
         return header
     }
     
@@ -115,6 +126,26 @@ class CalendarViewController: UIViewController, UICollectionViewDataSource, UICo
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return numberOfDaysInMonth[currentMonthIndex]
 //        return numberOfDaysInMonth[currentMonthIndex - 1] + firstWeekDayOfMonth - 1
+    }
+
+    func getReservationDateBoolean() {
+        let urlString = "http://myrealtrip.hongsj.kr/travel-information/Berlin/5/calender"
+        Alamofire
+            .request(urlString, method: .get)
+            .responseData { response in
+                switch response.result {
+                case .success(let data):
+                    do {
+                        let result = try JSONDecoder().decode([Schedule].self, from: data)
+                        print(result)
+                        print(response)
+                    } catch {
+                        print(error)
+                    }
+                case .failure(let error):
+                    print(error)
+                }
+        }
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -129,15 +160,30 @@ class CalendarViewController: UIViewController, UICollectionViewDataSource, UICo
             if calendarDate < todayDate && currentYear == presentYear && currentMonthIndex == presentMonthIndex {
                 cell.isUserInteractionEnabled = false
                 cell.dateLabel.textColor = UIColor.lightGray
+                //TODO: Bool값을 받아 false이면 해당 날짜를 회색처리 및 선택 안되게 함
+                //불가능한 날짜 : 서버에서 Bool(false)로 받아와서 선택 안되도록 회색 처리
+                //(현재 날짜 이전은 무조건 선택 안되도록)
             } else {
                 cell.isUserInteractionEnabled = true
-//                cell.dateLabel.textColor = Style.activeCellLblColor
             }
         }
         return cell
     }
     
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.backgroundColor = ColorOfIsSelected.red
+//        if cell?.backgroundColor == ColorOfIsSelected.red {
+//            
+//        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.backgroundColor = UIColor.clear
+    }
 }
+
 extension Date {
     var weekday: Int {
         return Calendar.current.component(.weekday, from: self)
@@ -146,8 +192,6 @@ extension Date {
         return Calendar.current.date(from: Calendar.current.dateComponents([.year, .month], from: self))!
     }
 }
-
-
 
 extension String {
     static var dateFormatter: DateFormatter = {
@@ -160,6 +204,8 @@ extension String {
         return String.dateFormatter.date(from: self)
     }
 }
+
+
 
 
 
--- iOSTeamProject-MySmallTrip/iOSTeamProject-MySmallTrip/DateCell.swift
+++ iOSTeamProject-MySmallTrip/iOSTeamProject-MySmallTrip/DateCell.swift
@@ -9,6 +9,18 @@
 import UIKit
 
 class DateCell: UICollectionViewCell {
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//        layer.cornerRadius = 50
+//        layer.borderColor = UIColor.clear.cgColor
+//        layer.borderWidth = 0
+//        
+//        layer.masksToBounds = true
+//    }
+//
+//    required init?(coder aDecoder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
     
     @IBOutlet weak var dateLabel: UILabel!
 }
