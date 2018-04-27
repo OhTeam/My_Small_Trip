24b3f89b4cc3a8ee4d4155a76de5b52216f7f1e8
Yongseok Choi <clyksb0731@gmail.com>
2018-04-27 11:32:19 +0900

    Prepare updating auto-login by adding the process for logging out from each situation.

--- iOSTeamProject-MySmallTrip/iOSTeamProject-MySmallTrip/ProfileViewController.swift
+++ iOSTeamProject-MySmallTrip/iOSTeamProject-MySmallTrip/ProfileViewController.swift
@@ -446,9 +446,16 @@ class ProfileViewController: UIViewController {
                 
                 // dev
 //                if self.tabBarController?.presentingViewController?.presentingViewController is SignUpViewController {
+//                    
+//                    // from Sign Up
 //                    self.tabBarController?.presentingViewController?.presentingViewController?.presentingViewController?.dismiss(animated: true, completion: nil)
-//                } else {
+//                    
+//                } else if self.tabBarController?.presentingViewController?.presentingViewController is RootViewController {
+//                    // from Log In
 //                    self.tabBarController?.presentingViewController?.presentingViewController?.dismiss(animated: true, completion: nil)
+//                } else {
+//                    // from Automatic Log In
+//                    self.tabBarController?.presentingViewController?.dismiss(animated: true, completion: nil)
 //                }
                 
             }) { (error, code) in
